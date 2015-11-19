c8c4a5c96604e79d3f1e17c12371c82a1244ff00
Daniel Morgan <daniel.morgan@nublue.co.uk>
2015-11-19 17:09:35 +0000

    Started working on hand events

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
-import Hand from './Hand'
+window.addEventListener('load', initGame);
 
-console.log(new Hand('rock'));
+function initGame() {
+  let hands = [ document.getElementById('rock'), document.getElementById('paper'), document.getElementById('scissors') ];
+  
+  for (let hand of hands) {
+    hand.addEventListener('click', playHand);
+  }
+}
+
+function playHand(event) {
+  console.log(event, this);
+}

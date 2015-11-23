717a607a182adf75d421489031f87e2af1908010
Daniel Morgan <me@danielmorgan.co.uk>
2015-11-23 21:10:36 +0000

    Removed CSS transitions to increase mobile performance

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -6,6 +6,7 @@ function playAnimation(playerHand, aiHand, outcome) {
     scissors: 'rgba(45, 191, 155, 1)',
     paper: 'rgba(255, 215, 42, 1)'
   }
+  const hexRadius = 80;
 
   let container = document.getElementById('results');
   container.style.display = 'block';
@@ -20,7 +21,13 @@ function playAnimation(playerHand, aiHand, outcome) {
   let left = new Konva.Group();
   let right = new Konva.Group();
 
-  let hexRadius = 80;
+  let backgroundFill = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: stage.width(),
+    height: stage.height(),
+    fill: 'rgba(255, 255, 255, 0.75)'
+  });
   let hexL = new Konva.RegularPolygon({
     x: -hexRadius / 2,
     y: stage.height() / 2 - hexRadius / 2,
@@ -56,39 +63,33 @@ function playAnimation(playerHand, aiHand, outcome) {
     shadowOpacity: 0.4,
     cornerRadius: 2
   });
-  let rect = new Konva.Rect({
-    x: 0,
-    y: 0,
-    width: stage.width(),
-    height: stage.height(),
-    fill: 'rgba(255, 255, 255, 0.5)'
-  });
 
   left.add(hexL);
   right.add(hexR);
-  background.add(rect, left, right);
+  background.add(backgroundFill, left, right);
   foreground.add(textBox, text);
   stage.add(background, foreground);
 
-  let animation = new Konva.Animation(function(frame) {
+  new Konva.Animation(function(frame) {
     hexL.setX(frame.time / 2);
     hexR.setX(stage.width() - frame.time / 2);
 
     if (hexL.x() >= stage.width() / 2 - hexRadius) {
       this.stop();
     }
-  }, stage);
+  }, stage).start();
 
-  animation.start();
-
-  let showResult = setTimeout(function() {
+  let waitThenHideResult = setTimeout(function() {
     container.style.display = 'none';
   }, 2000);
 
-  container.addEventListener('click', function(){
-    clearTimeout(showResult);
+  function hideResultNow() {
+    clearTimeout(waitThenHideResult);
     this.style.display = 'none';
-  });
+  }
+
+  container.addEventListener('click', hideResultNow);
+  container.addEventListener('touchstart', hideResultNow);
 }
 
 export default playAnimation;
